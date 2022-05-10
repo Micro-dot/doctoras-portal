ac568f6c2b47744c8b284822bec4077de1c4decb
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-05-10 18:48:45 +0600

    create-react-folder & setup pages

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Navber from './Pages/Shared/Navber';
+
+function App() {
+  return (
+    <div>
+      <Navber></Navber>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/' element={<About></About>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
