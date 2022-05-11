488f56dfb13e0a4f7739f5a0183582c0cb667152
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-05-11 17:41:03 +0600

    services added

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -3,8 +3,8 @@ import chair from '../../assets/images/chair.png';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
 import { format } from 'date-fns';
-const AppointmentBanner = () => {
-    const [date, setDate] = useState(new Date());
+const AppointmentBanner = ({ date, setDate }) => {
+
     return (
         <div className="hero min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -15,7 +15,6 @@ const AppointmentBanner = () => {
                         selected={date}
                         onSelect={setDate}
                     />
-                    <p>You Have selected : {format(date, 'PP')}</p>
                 </div>
             </div>
 
