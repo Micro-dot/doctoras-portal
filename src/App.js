7f00c88b50b3fab3027a9a5a2baa87d64500cfda
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-05-11 12:10:40 +0600

    Set calendar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import Navber from './Pages/Shared/Navber';
 
 function App() {
   return (
-    <div>
+    <div className='max-w-7xl mx-auto'>
       <Navber></Navber>
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/about' element={<About/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/appointment' element={<Appointment />}></Route>
+        <Route path='/login' element={<Login />}></Route>
       </Routes>
     </div>
   );
--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -12,10 +12,10 @@ const MakeAppointment = () => {
             <div className='flex-1 hidden lg:block'>
                 <img className='mt-[-120px]' src={doctor} alt="" />
             </div>
-            <div className='flex-1'>
+            <div className='flex-1 px-5'>
                 <h3 className='text-xl text-primary font-bold' >Appointment</h3>
-                <h2 className='text-3xl text-white'>Make an Appointment Today</h2>
-                <p className='text-white'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium, officia commodi! Tenetur nesciunt reprehenderit laboriosam architecto corrupti quaerat aliquam eaque adipisci commodi, odio temporibus dolor! Quos ratione, inventore eum molestias corrupti quasi? Sed quis reprehenderit voluptatem ipsa? Sunt, explicabo iusto?</p>
+                <h2 className='text-3xl text-white py-5'>Make an Appointment Today</h2>
+                <p className='text-white pb-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium, officia commodi! Tenetur nesciunt reprehenderit laboriosam architecto corrupti quaerat aliquam eaque adipisci commodi, odio temporibus dolor! Quos ratione, inventore eum molestias corrupti quasi? Sed quis reprehenderit voluptatem ipsa? Sunt, explicabo iusto?</p>
                 <PrimaryButton>Get Strated</PrimaryButton>
             </div>
         </section>
