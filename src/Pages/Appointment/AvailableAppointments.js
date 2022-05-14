3f976dec4fb88cc422d94728a186b8017c0e4927
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-05-15 00:24:22 +0600

    MultiUser booking appointment

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -2,21 +2,30 @@ import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
 import BookingModal from './BookingModal';
 import Service from './Service';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import Loading from '../Shared/Loading';
 const AvailableAppointments = ({ date }) => {
-    const [services, setServices] = useState([]);
+    // const [services, setServices] = useState([]);
     const [treatment, setTreatment] = useState(null);
     const formattedDate = format(date, 'PP')
-    useEffect(() => {
+    // useEffect(() => {
+    //     fetch(`http://localhost:5000/available?date=${formattedDate}`)
+    //         .then(res => res.json())
+    //         .then(data => setServices(data));
+    // }, [formattedDate])
+    const { data: services, isLoading, refetch } = useQuery(['available', formattedDate], () =>
         fetch(`http://localhost:5000/available?date=${formattedDate}`)
             .then(res => res.json())
-            .then(data => setServices(data));
-    }, [formattedDate])
+    )
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
             <h4 className='text-xl text-secondary text-center my-12'>Available Appointment on {format(date, 'PP')}</h4>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
-                    services.map(service => <Service
+                    services?.map(service => <Service
                         key={service._id}
                         service={service}
                         setTreatment={setTreatment}
@@ -26,7 +35,10 @@ const AvailableAppointments = ({ date }) => {
             {treatment && <BookingModal
                 date={date}
                 treatment={treatment}
-                setTreatment={setTreatment}>
+                setTreatment={setTreatment}
+                refetch={refetch}
+                >
+                
             </BookingModal>}
         </div>
     );
--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -4,9 +4,9 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { toast } from 'react-toastify';
 const BookingModal = ({ date, treatment, setTreatment }) => {
-    const [user, loading, error] = useAuthState(auth);
-    const formattedDate = format(date, "PP")
     const { _id, name, slots } = treatment;
+    const [user, loading, error] = useAuthState(auth);
+    const formattedDate = format(date, 'PP');
     const handleBooking = event => {
         event.preventDefault();
         const slot = event.target.slot.value;
@@ -16,31 +16,28 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
             treatment: name,
             date: formattedDate,
             slot,
-            patientEmail: user.email,
+            patient: user.email,
             patientName: user.displayName,
             patientPhone: event.target.phone.value
         }
-        console.log(booking);
-
+        // console.log(booking);
         fetch('http://localhost:5000/booking', {
-            method: "POST",
+            method: 'POST',
             headers: {
                 'content-type': 'application/json'
             },
             body: JSON.stringify(booking)
-        })  
+        })
             .then(res => res.json())
             .then(data => {
-                console.log(data);
-                if (data.success) {
-                    toast(`Appointment is set ${formattedDate} at ${slot}`)
+                if(data.success){
+                    toast(`Appointment is set, ${formattedDate} at ${slot}`)
                 }
-                else {
-                    toast.error(`Already have an appointment on ${data.booking?.date} at ${data.booking?.slot}`)
+                else{
+                    toast.error(`Already have and appointment on ${data.booking?.date} at ${data.booking?.slot}`)
                 }
                 setTreatment(null);
-            })
-
+            });
     }
     return (
         <div>
@@ -54,8 +51,8 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
                         <select name="slot" className="select select-bordered w-full max-w-xs">
                             {slots.map((slot, i) => <option value={slot} key={i} >{slot}</option>)}
                         </select>
-                        <input type="text" name='name' disabled value={user?.displayName || ""} className="input input-bordered w-full max-w-xs" />
-                        <input type="text" name='email' disabled value={user?.email} className="input input-bordered w-full max-w-xs" />
+                        <input type="text" name='name' disabled value={user?.displayName || " "} className="input input-bordered w-full max-w-xs" />
+                        <input type="text" name='email' disabled value={user?.email || " "} className="input input-bordered w-full max-w-xs" />
                         <input type="text" name='phone' placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
                         <input type="submit" value="Submit" className="btn btn-secondary w-full max-w-xs" />
                     </form>
@@ -66,3 +63,4 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
 };
 
 export default BookingModal;
+
