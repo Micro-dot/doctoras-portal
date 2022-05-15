881c9fcd3749b6c2c0fee0247c34193fbbf97449
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-05-15 23:15:12 +0600

    Make Admin

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
-const UserRow = ({ user }) => {
-    const { email } = user;
+const UserRow = ({ user, refetch }) => {
+    const { email, role } = user;
+    const makeAmin = () => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: "PUT",
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                toast.success('Successfully Made an admin');
+                refetch();
+            })
+    }
     return (
         <tr>
             <th>1</th>
             <td>{email}</td>
-            <td><button class="btn btn-xs">Make Admin</button></td>
+            <td>{role !== 'admin' && <button onClick={makeAmin} class="btn btn-xs">Make Admin</button>}</td>
             <td><button class="btn btn-xs">Remove User</button></td>
         </tr>
     );
