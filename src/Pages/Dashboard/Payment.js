8ff478b5fd82b428b811923ec2f682629acd8e21
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-05-19 18:19:52 +0600

    Payment form added

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,7 +1,12 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../Shared/Loading';
+import CheckOutForm from './CheckOutForm';
+
+const stripePromise = loadStripe('pk_test_51L17wJB5gQDzsDTV794q6qLFlXPE2P4Tnyb2QFzj6HNxxtsEVDs8ycmo3F0ZNkf88FvUjQU2AC6x9YVLaGHw5jVi00xhXP16On');
 
 const Payment = () => {
     const { id } = useParams();
@@ -28,6 +33,9 @@ const Payment = () => {
             </div>
             <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
                 <div class="card-body">
+                    <Elements stripe={stripePromise}>
+                        <CheckOutForm></CheckOutForm>
+                    </Elements>
                 </div>
             </div>
         </div>
