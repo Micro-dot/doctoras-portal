878d40c570a10c6349d7633b2a363f17f9afb35e
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-05-19 19:38:09 +0600

    payment btn addded

--- src/Pages/Dashboard/CheckOutForm.js
+++ src/Pages/Dashboard/CheckOutForm.js
@@ -1,10 +1,29 @@
 import { async } from '@firebase/util';
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const CheckOutForm = () => {
+const CheckOutForm = ({ appointment }) => {
     const stripe = useStripe();
     const elements = useElements();
+    const [cardError, setCardError] = useState('');
+    const [clientSecret, setClientSecret] = useState("");
+    const { price } = appointment
+    useEffect(() => {
+        fetch('http://localhost:5000/create-payment-intent', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            },
+            body: JSON.stringify({ price })
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data?.clientSecret) {
+                    setClientSecret(data.clientSecret);
+                }
+            });
+    }, [])
     const handleSubmit = async (event) => {
         event.preventDefault();
         if (!stripe || !elements) {
@@ -14,30 +33,55 @@ const CheckOutForm = () => {
         if (card == null) {
             return
         }
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        })
+
+        setCardError(error?.message || '')
+
+        //confirm Card payment 
+        const { paymentIntent, error:intentError } = await stripe.confirmCardPayment(
+          clientSecret,
+            {
+                payment_method: {
+                    card: cardElement,
+                    billing_details: {
+                        name: 'Jenny Rosen',
+                    },
+                },
+            },
+        );
+
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+        <>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
                             },
                         },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe}>
-                Pay
-            </button>
-        </form>
+                    }}
+                />
+                <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe || !clientSecret}>
+                    Pay
+                </button>
+            </form>
+            {
+                cardError && <p className='text-red-500'>{cardError}</p>
+            }
+        </>
     );
 };
 
--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -34,7 +34,10 @@ const Payment = () => {
             <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
                 <div class="card-body">
                     <Elements stripe={stripePromise}>
-                        <CheckOutForm></CheckOutForm>
+                        <CheckOutForm
+                            appointment={appointment}
+                        ></CheckOutForm>
+
                     </Elements>
                 </div>
             </div>
